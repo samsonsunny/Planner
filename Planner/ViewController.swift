c4d0bc00174e4d6c56b8a5dbd832a09aa35789b7
samson <sam.sunny@a-cti.com>
2019-01-16 18:30:14 +0530

    First Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by Sam on 1/15/19.
+//  Copyright © 2019 Mulanthanam. All rights reserved.
+//
+
+import UIKit
+import CalendarFramework
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		CalendarSetupController.shared.printHello()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		CalendarSetupController.shared.printHi()
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		CalendarSetupController.shared.printBye()
+	}
+
+
+}
+
